08dc10e31c6da6097f57f7b421acd0bfc9a34a5a
Tony Knight <tony.knight@newday.co.uk>
2025-05-19 09:36:26 +0100

    WIP

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
+﻿using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.RateLimiting;
+using Yarp.ReverseProxy.Transforms;
+using YarpTest1;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,11 +9,49 @@ builder.Services.AddControllers();
 // Add the reverse proxy capability to the server
 builder.Services.AddReverseProxy()
     // Initialize the reverse proxy from the "ReverseProxy" section of configuration
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    //.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    .AddTransforms(ctx =>
+    {
+        // TODO: how to only apply this transform to selected requests/destinations?
+        
+        ctx.AddRequestTransform(async reqCtx =>
+        {
+            // TODO: this is executed on EVERY request!
+            reqCtx.ProxyRequest.Headers.Authorization = null;
+            // reqCtx.DestinationPrefix has destination... but what happens when in multiple routes?
+            await Task.Delay(0); // TODO: 
+
+            // for the PR introducing rate limiting to YARP: https://github.com/dotnet/yarp/pull/1967/files
+            // https://learn.microsoft.com/en-gb/aspnet/core/performance/rate-limit?view=aspnetcore-9.0
+
+            return;
+        });
+    })
+    
+    .LoadFromMemory(ProgramBootstrap.GetRoutes(), ProgramBootstrap.GetClusters());
+// To kick a refresh: https://learn.microsoft.com/en-us/aspnet/core/fundamentals/servers/yarp/config-providers?view=aspnetcore-9.0#in-memory-config
+// httpContext.RequestServices.GetRequiredService<InMemoryConfigProvider>().Update(routes, clusters);
+
+
+builder.Services.AddRateLimiter(opt =>
+{
+    opt.AddFixedWindowLimiter("oneRequestPer10Secs", o2 =>
+    {
+        // TODO: partition key???
+        o2.PermitLimit = 20;
+        o2.QueueLimit = 1;
+        o2.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        o2.Window = TimeSpan.FromSeconds(10);
+    });
+    opt.RejectionStatusCode = 429;
+});
+builder.WebHost.UseUrls("http://localhost:5010");
 
 var app = builder.Build();
 
 // Register the reverse proxy routes
+ app.UseRateLimiter();
 app.MapReverseProxy();
 
 app.Run();
+
---
+++ ProgramBootstrap.cs
@@ -0,0 +1,74 @@
+﻿using Yarp.ReverseProxy.Configuration;
+
+namespace YarpTest1
+{
+
+    public static class ProgramBootstrap
+    {
+        const string DEBUG_HEADER = "Debug";
+        const string DEBUG_METADATA_KEY = "debug";
+        const string DEBUG_VALUE = "true";
+
+        public static RouteConfig[] GetRoutes()
+        {
+            return
+            [
+                new RouteConfig()
+                {
+                    RouteId = "route",
+                    ClusterId = "cluster1",
+                    RateLimiterPolicy = "oneRequestPer10Secs",
+                    Match = new RouteMatch
+                    {
+                        // Path or Hosts are required for each route. This catch-all pattern matches all request paths.
+                        Path = "{**catch-all}"
+                    }
+                }
+            ];
+        }
+
+        public static ClusterConfig[] GetClusters()
+        {
+            return
+            [
+                new ClusterConfig()
+                {
+                    ClusterId = "cluster1",
+                    HealthCheck = new HealthCheckConfig()
+                    {
+                        Active = new ActiveHealthCheckConfig()
+                        {
+                            Enabled = true,
+                            Interval = TimeSpan.FromSeconds(5),
+                            Timeout = TimeSpan.FromSeconds(30),
+                            Policy = "ConsecutiveFailures", // Docs = ??
+                            Path = "/heartbeat",
+                        },
+                    },
+                    Metadata = new Dictionary<string, string>()
+                    {
+                        { "ConsecutiveFailuresHealthPolicy.Threshold", "3" } // TODO: mapping to above?
+                    },
+                    LoadBalancingPolicy = "Random", // docs: https://learn.microsoft.com/en-us/aspnet/core/fundamentals/servers/yarp/load-balancing?view=aspnetcore-9.0
+                    Destinations = new Dictionary<string, DestinationConfig>(StringComparer.OrdinalIgnoreCase)
+                    {
+                        {
+                            "destination1-primary", new DestinationConfig()
+                            {
+                                Address = "http://localhost:5000", 
+                                // TODO: authentication via requesttransforms? https://learn.microsoft.com/en-us/aspnet/core/fundamentals/servers/yarp/extensibility-transforms?view=aspnetcore-9.0
+                            }
+                        },
+                        {
+                            "destination1-secondary", new DestinationConfig()
+                            {
+                                Address = "http://127.0.0.1:5000",
+                                // TODO: authentication via requesttransforms? https://learn.microsoft.com/en-us/aspnet/core/fundamentals/servers/yarp/extensibility-transforms?view=aspnetcore-9.0
+                            }
+                        },
+                    }
+                }
+            ];
+        }
+    }
+}
